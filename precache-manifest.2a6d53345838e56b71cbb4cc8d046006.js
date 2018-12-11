bf3229ea71cc65032e4afc60a90e0733ab041cff
ddemolina <ddemolina@bizpills.es>
2018-12-11 13:12:53 +0100

    Updates

---
+++ precache-manifest.2a6d53345838e56b71cbb4cc8d046006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d82e32a7ed723e721fd",
+    "url": "/select-libs/static/js/runtime~main.4d82e32a.js"
+  },
+  {
+    "revision": "ed4c533f64d10309574d",
+    "url": "/select-libs/static/js/main.ed4c533f.chunk.js"
+  },
+  {
+    "revision": "ad99f7c6d1346afbc067",
+    "url": "/select-libs/static/js/1.ad99f7c6.chunk.js"
+  },
+  {
+    "revision": "ed4c533f64d10309574d",
+    "url": "/select-libs/static/css/main.e0ac20fe.chunk.css"
+  },
+  {
+    "revision": "ad99f7c6d1346afbc067",
+    "url": "/select-libs/static/css/1.67e3b2a6.chunk.css"
+  },
+  {
+    "revision": "b90da1bce5f005b99b1076cc38eb5b6b",
+    "url": "/select-libs/index.html"
+  }
+];
