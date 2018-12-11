f235d0dad14f8b2a848b4e107fa0838ca0f17f33
ddemolina <ddemolina@bizpills.es>
2018-12-11 13:10:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Select} from 'antd';
+import 'antd/dist/antd.css';
 
-class App extends Component {
-  render() {
+const Option = Select.Option;
+
+class App extends Component
+{
+  render()
+  {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Select className="main" placeholder="elige algo" showSearch={true}>
+          <Option value="Bahamas">Bahamas</Option>
+          <Option value="Camboya">Camboya</Option>
+          <Option value="Islas Caimán">Islas Caimán</Option>
+          <Option value="Dinamarca">Dinamarca</Option>
+          <Option value="Ecuador">Ecuador</Option>
+          <Option value="España">España</Option>
+          <Option value="Finlandia">Finlandia</Option>
+          <Option value="Gambia">Gambia</Option>
+          <Option value="Haití">Haití</Option>
+          <Option value="India">India</Option>
+          <Option value="Kenya">Kenya</Option>
+          <Option value="Líbano">Líbano</Option>
+          <Option value="Malasia">Malasia</Option>
+          <Option value="Nepal">Nepal</Option>
+          <Option value="Omán">Omán</Option>
+          <Option value="Panamá">Panamá</Option>
+          <Option value="Qatar">Qatar</Option>
+          <Option value="Rusia">Rusia</Option>
+          <Option value="Samoa">Samoa</Option>
+          <Option value="Túnez">Túnez</Option>
+          <Option value="Uganda">Uganda</Option>
+          <Option value="Venezuela">Venezuela</Option>
+          <Option value="Wallis y Futuna">Wallis y Futuna</Option>
+          <Option value="Zambia">Zambia</Option>
+        </Select>
+
     );
   }
 }
